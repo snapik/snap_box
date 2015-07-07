102fc075580d7800c3a038ee527b16b571d81f3d
snap <a.sus@internetdevels.com>
2015-07-07 12:21:24 +0300

    Add all files

---
+++ drupal/reinstall.sh
@@ -0,0 +1,5 @@
+if [ "${1}"=="--windows" ]; then
+ time ansible-playbook -vvvv reinstall.yml -i 'localhost,' --connection=local --extra-vars "is_windows=true"
+else
+ time ansible-playbook -vvvv reinstall.yml -i 'localhost,' --connection=local
+fi
